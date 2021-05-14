87a20b0f821a1f292059fa4b6882c4627eb858f4
Cazac <zachary.blouin@gmail.com>
2021-05-13 18:05:48 -0700

    Initial Commit

---
+++ TTGD - Gobo Town/Assets/_Scripts/HexCell.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HexCell : MonoBehaviour
+{
+    ////////////////////////////////
+
+    public enum HexDirection
+    {
+        N, NE, SE, S, SW, NW
+    }
+
+    ////////////////////////////////
+
+    [SerializeField]
+    HexCell[] neighbors;
+
+
+
+    public HexCoords hexCoords;
+
+    public TextMeshProUGUI hexLabel_Text;
+
+  
+
+
+    public GameObject HexObject;
+
+
+    [Header("Colors")]
+    public Color colorActive;
+    public Color colorHover = Color.white;
+
+    public Color colorRangeMin;
+    public Color colorRangeMax;
+
+
+    private Renderer hexRenderer;
+    private MaterialPropertyBlock matPropBlock;
+
+
+    public HexDirection GetOppositeDirection(HexDirection direction)
+    {
+        int directionValue = (int)direction;
+
+        if (directionValue < 3)
+        {
+            return direction + 3;
+        }
+        else
+        {
+            return direction - 3;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    private void Awake()
+    {
+        matPropBlock = new MaterialPropertyBlock();
+        hexRenderer = HexObject.GetComponent<Renderer>();
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)GetOppositeDirection(direction)] = this;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public void SetLabel(int x, int z)
+    {
+        hexCoords = HexCoords.GenerateCoords(x, z);
+        string hexID = (x.ToString() + "/" + z.ToString());
+
+        hexLabel_Text.text = hexID;
+        gameObject.name = hexID;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public void TouchCell()
+    {
+        Debug.Log("Touched Hex Cell: " + hexCoords.X + " - " + hexCoords.Z);
+    }
+
+    public void ClickCell()
+    {
+        Debug.Log("Click Hex Cell: " + hexCoords.X + " - " + hexCoords.Z);
+
+
+        UpdateCellColor(colorHover);
+        
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public void GenerateCellColor()
+    {
+        float value = Random.Range(0, 1f);
+        colorActive = Color.Lerp(colorRangeMin, colorRangeMax, value);
+    }
+
+    public void UpdateCellColor(Color newColor)
+    {
+        // Get the current value of the material properties in the renderer.
+        hexRenderer.GetPropertyBlock(matPropBlock);
+        // Assign our new value.
+        matPropBlock.SetColor("_Color", newColor);
+        // Apply the edited values to the renderer.
+        hexRenderer.SetPropertyBlock(matPropBlock);
+    }
+
+    /////////////////////////////////////////////////////////////////
+}
+
+
+
+
---
+++ TTGD - Gobo Town/Assets/_Scripts/HexCoords.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct HexCoords
+{
+
+    [SerializeField]
+    private int x;
+    [SerializeField]
+    private int y;
+    [SerializeField]
+    private int z;
+    [SerializeField]
+    private int l;
+
+    public int X { get { return x; } }
+    public int Y { get { return y; } }
+    public int Z { get { return z; } }
+    public int L { get { return l; } }
+ 
+
+    public static HexCoords GenerateCoords(int x, int y)
+    {
+        return new HexCoords(x, y);
+    }
+
+    [HideInInspector]
+    public HexCoords(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        z = 0;
+        l = 0;
+    }
+
+    public string GetPrintableCoords()
+    {
+        string hexCoordInfo_X = "X: " + x;
+        string hexCoordInfo_Y = "Y: " + y;
+        string hexCoordInfo_Z = "Z: " + z;
+        string hexCoordInfo_L = "Level: " + l;
+
+        return "Hex Coords: (" + hexCoordInfo_X + " - " + hexCoordInfo_Y + " - " + hexCoordInfo_Z + " - " + hexCoordInfo_L + ")";
+    }
+}
